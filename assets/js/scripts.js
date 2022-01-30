a4b8e2fe68fd26853a8e9afee2f4564bdbc6675c
jasurka <sadullaevjasur12@gmail.com>
2022-01-30 20:25:02 +0500

    added new scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 let modalForm = document.querySelector('.modal-login')
 let overlay = document.querySelector('.overlay')
+let videoUrl = document.querySelector('.video-inner iframe').getAttribute('src')
 
 document.querySelector('.login').addEventListener('click', function (){
     //e.preventDefault()
@@ -9,8 +10,22 @@ document.querySelector('.login').addEventListener('click', function (){
 overlay.addEventListener('click', function (){
     modalForm.classList.remove('show')
     overlay.classList.remove('show')
+    document.querySelector('.video-inner iframe').setAttribute('src', videoUrl)
 })
 document.querySelector('.close-button').addEventListener('click',function (){
     modalForm.classList.remove('show')
     overlay.classList.remove('show')
+})
+
+document.querySelector('.video-button').addEventListener('click', function(){
+    overlay.classList.add('show')
+    document.querySelector('.video-modal').classList.add('show')
+})
+
+jQuery('.classes-row').slick({
+    infinite:true,
+    slidesToShow:3,
+    dots:true,
+    arrows:false,
+    margin:20,
 })
